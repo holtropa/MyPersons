503db73945954ec33f1b2aa07e3f91f82897a326
Anne Holtrop <Anne.Holtrop@rabobank.nl>
2017-07-07 14:50:14 +0200

    test

--- src/main/java/nl/rabobank/personenbeheer/H2MemoryDatabase.java
+++ src/main/java/nl/rabobank/personenbeheer/H2MemoryDatabase.java
@@ -29,9 +29,13 @@ public class H2MemoryDatabase {
         PreparedStatement insertPreparedStatement = null;
         PreparedStatement selectPreparedStatement = null;
 
-        String CreateQuery = "CREATE TABLE PERSOON(id int primary key, name varchar(255))";
-        String InsertQuery = "INSERT INTO PERSOON" + "(id, name) values" + "(?,?)";
-        String SelectQuery = "select * from PERSOON";
+        String CreateQuery = "CREATE TABLE XZA_PERSOON(id int primary key, name varchar(255))";
+
+     //   String CreateQuery = "CREATE TABLE XZA_PERSOON(ID int primary key, ACHTERNAAM varchar(255), BSNNUMMER varchar(255), EMAIL varchar(255), GESLACHT varchar(255), TELEFOON=NUMMER varchar(255), TUSSENVOEGSELS varchar(255)"
+      //      + ", VOORLETTERS varchar(255), HUISNUMMER varchar(255), HUISNUMMERTOEVOEGING varchar(255), LAND varchar(255), POSTCODE varchar(255), STRAAT varchar(255)"
+     //      + ", WOONPLAATS varchar(255), GEBOORTEPLAATS varchar(255), GEMEENTE varchar(255)";
+        String InsertQuery = "INSERT INTO XZA_PERSOON" + "(id, name) values" + "(?,?)";
+        String SelectQuery = "select * from XZA_PERSOON";
 
         try {
             connection.setAutoCommit(false);
@@ -70,18 +74,18 @@ public class H2MemoryDatabase {
         try {
             connection.setAutoCommit(false);
             stmt = connection.createStatement();
-            stmt.execute("CREATE TABLE PERSOON(id int primary key, name varchar(255))");
-            stmt.execute("INSERT INTO PERSOON(id, name) VALUES(1, 'Anju')");
-            stmt.execute("INSERT INTO PERSOON(id, name) VALUES(2, 'Sonia')");
-            stmt.execute("INSERT INTO PERSOON(id, name) VALUES(3, 'Asha')");
+            stmt.execute("CREATE TABLE XZA_PERSOON(id int primary key, name varchar(255))");
+            stmt.execute("INSERT INTO XZA_PERSOON(id, name) VALUES(1, 'Anju')");
+            stmt.execute("INSERT INTO XZA_PERSOON(id, name) VALUES(2, 'Sonia')");
+            stmt.execute("INSERT INTO XZA_PERSOON(id, name) VALUES(3, 'Asha')");
 
-            ResultSet rs = stmt.executeQuery("select * from PERSOON");
+            ResultSet rs = stmt.executeQuery("select * from XZA_PERSOON");
             System.out.println("H2 In-Memory Database inserted through Statement");
             while (rs.next()) {
                 System.out.println("Id " + rs.getInt("id") + " Name " + rs.getString("name"));
             }
 
-            stmt.execute("DROP TABLE PERSOON");
+            stmt.execute("DROP TABLE XZA_PERSOON");
             stmt.close();
             connection.commit();
         } catch (SQLException e) {
@@ -93,7 +97,8 @@ public class H2MemoryDatabase {
         }
     }
 
-    private static Connection getDBConnection() {
+  //  private static Connection getDBConnection() {\
+    public static Connection getDBConnection() {
         Connection dbConnection = null;
         try {
             Class.forName(DB_DRIVER);
--- src/test/java/nl/rabobank/personenbeheer/IntegratieTest.java
+++ src/test/java/nl/rabobank/personenbeheer/IntegratieTest.java
@@ -2,7 +2,9 @@ package nl.rabobank.personenbeheer;
 
 import org.dbunit.*;
 import org.dbunit.database.DatabaseConfig;
+import org.dbunit.database.DatabaseConnection;
 import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.CompositeDataSet;
 import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
 import org.dbunit.operation.DatabaseOperation;
@@ -22,98 +24,54 @@ import static org.junit.Assert.assertNull;
 
 public class IntegratieTest extends DBTestCase {
 
-//    public IntegratieTest(String name)
-//    {
-//        super(name);
-//    }
 
-    public IntegratieTest(String name)
-    {
-        super( name );
-//        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS, "org.hsqldb.jdbcDriver" );
-//        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL, "jdbc:hsqldb:sample" );
-//        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_USERNAME, "sa" );
-//        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_PASSWORD, "" );
-        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS, "org.h2.Driver" );
-        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL, "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1" );
-        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_USERNAME, "" );
-        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_PASSWORD, "" );
-        H2MemoryDatabase mydbase = new H2MemoryDatabase();
-      //  H2MemoryDatabase.main();
-       // H2MemoryDatabase.insertWithPreparedStatement();
-       // mydbase.main(["",""]);
-        // System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_SCHEMA, "" );
-//        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS, "org.hsqldb.jdbcDriver" );
-//        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL, "database.connection.url" );
-//        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_USERNAME, "database.connection.user" );
-//        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_PASSWORD, "database.connection.password" );
-//        // System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_SCHEMA, "" );
+    public IntegratieTest(String name) {
+        super(name);
+        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS, "org.h2.Driver");
+        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL, "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1");
+        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_USERNAME, "");
+        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_PASSWORD, "");
     }
 
-    protected IDataSet getDataSet() throws Exception
-    {
+    protected IDataSet getDataSet() throws Exception {
 
         File file = new File(getClass().getClassLoader().getResource("export.xml").getFile());
         return new FlatXmlDataSetBuilder().build(new FileInputStream(file));
 
     }
 
-    /**
-     * Override method to set custom properties/features
-     */
     protected void setUpDatabaseConfig(DatabaseConfig config) {
         config.setProperty(DatabaseConfig.PROPERTY_BATCH_SIZE, new Integer(97));
         config.setFeature(DatabaseConfig.FEATURE_BATCHED_STATEMENTS, true);
     }
 
-//    @Before
-//    public void setUp() throws Exception
-//    {
-//
-//        // initialize your database connection here
-//        IDatabaseConnection connection = null;
-//        // ...
-//
-//        // initialize your dataset here
-//        IDataSet dataSet = null;
-//        // ...
-//
-//        try
-//        {
-//            DatabaseOperation.CLEAN_INSERT.execute(connection, dataSet);
-//        }
-//        finally
-//        {
-//            connection.close();
-//        }
-//    }
+    @Before
+    public void setUp() throws Exception {
 
-//    @Override
-//    protected DatabaseOperation getSetUpOperation() throws Exception
-//    {
-//        dbConnection = DriverManager.getConnection("jdbc:hsqldb:mem:tests", "sa", "");
-//        createTables();
-//        return DatabaseOperation.REFRESH;
-//    }
-//
-//    private void createTables() throws Exception {
-//
-//        PreparedStatement statement = dbConnection
-//                .prepareStatement("CREATE TABLE digidauthsessions ( "
-//                        + "id INTEGER IDENTITY NOT NULL PRIMARY KEY,"
-//                        + "request_token VARCHAR(50) NOT NULL,"
-//                        + "response_token VARCHAR(50) NOT NULL,"
-//                        + "relatienr VARCHAR(50) NOT NULL,"
-//                        + "digidnameid VARCHAR(1000) NOT NULL" + ")");
-//        statement.execute();
-//        statement.close();
-//    }
+        // initialize your database connection here
+        H2MemoryDatabase mydbase = new H2MemoryDatabase();
+        IDatabaseConnection con = new DatabaseConnection(mydbase.getDBConnection());
+
+        // initialize your dataset here
+        FlatXmlDataSetBuilder builder = new FlatXmlDataSetBuilder();
+        builder.setColumnSensing(true);
+        IDataSet datasets = getDataSet();
+
+
+        DatabaseOperation.REFRESH.execute(con, new CompositeDataSet(datasets)); // Import
+
+
+        try {
+            DatabaseOperation.CLEAN_INSERT.execute(con, datasets);
+        } finally {
+            con.close();
+        }
+    }
 
     private PrepAndExpectedTestCase tc; // injected or instantiated, already configured
 
     @Test
-    public void testExample() throws Exception
-    {
+    public void testExample() throws Exception {
         final String[] prepDataFiles = {}; // define prep files
         final String[] expectedDataFiles = {}; // define expected files
         final VerifyTableDefinition[] tables = {}; // define tables to verify
@@ -124,17 +82,5 @@ public class IntegratieTest extends DBTestCase {
         };
 
         tc.runTest(tables, prepDataFiles, expectedDataFiles, testSteps);
-    }
-
-    @Test
-    public void write_a_person_to_dbase() {
-        WritePersoonToDbase writePersoonToDbase = new WritePersoonToDbase();
-        Persoon persoon = new Persoon("Piet", LocalDate.of(1965, 12, 12));
-        PersoonInvoer.personen.add(persoon);
-        int itId=persoon.getId();
-        writePersoonToDbase.storePersoon(persoon);
-        int zoekId = writePersoonToDbase.selectPersoon(persoon);
-        assertEquals(zoekId,PersoonInvoer.personen.get(0).getId());
-        writePersoonToDbase.removePersoon(persoon);
     }
 }
