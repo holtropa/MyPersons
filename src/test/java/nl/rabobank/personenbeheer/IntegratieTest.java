cb762102984344a112998b5df10a270c2816ff8f
Anne Holtrop <Anne.Holtrop@rabobank.nl>
2017-07-14 12:55:52 +0200

    test

--- src/test/java/nl/rabobank/personenbeheer/IntegratieTest.java
+++ src/test/java/nl/rabobank/personenbeheer/IntegratieTest.java
@@ -15,10 +15,7 @@ import org.junit.Test;
 import java.io.File;
 import java.io.FileInputStream;
 import java.math.BigInteger;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.time.LocalDate;
 
 import static nl.rabobank.personenbeheer.H2MemoryDatabase.getDBConnection;
@@ -57,8 +54,8 @@ public class IntegratieTest extends DBTestCase {
 
         DatabaseConfig dbConfig = con.getConfig();
 
-        // added this line to get rid of the warning
-        dbConfig.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new OracleDataTypeFactory());
+                                                            // added this line to get rid of the warning
+                                                            dbConfig.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new OracleDataTypeFactory());
 
         // initialize your dataset here
         FlatXmlDataSetBuilder builder = new FlatXmlDataSetBuilder();
@@ -66,12 +63,15 @@ public class IntegratieTest extends DBTestCase {
         IDataSet datasets = getDataSet();
 
         this.createTable();
+ //       this.insertTable();
+  //      this.selectTable();
 
-        DatabaseOperation.REFRESH.execute(con, new CompositeDataSet(datasets)); // Import
+  //      DatabaseOperation.REFRESH.execute(con, new CompositeDataSet(datasets)); // Import
 
 
         try {
             DatabaseOperation.CLEAN_INSERT.execute(con, datasets);
+            this.selectTable();
         } finally {
             con.close();
         }
@@ -81,11 +81,15 @@ public class IntegratieTest extends DBTestCase {
 
     @Test
     public void testExample() throws Exception {
-        final String[] prepDataFiles = {}; // define prep files
-        final String[] expectedDataFiles = {}; // define expected files
+        final String[] prepDataFiles = {"export2.xml"}; // define prep files
+        final String[] expectedDataFiles = {"1","Anne"}; // define expected files
         final VerifyTableDefinition[] tables = {}; // define tables to verify
         final PrepAndExpectedTestCaseSteps testSteps = () -> {
             // execute test steps
+            insertTable("1", "Anne");
+            insertTable("2", "Piet");
+            insertTable("3", "Henk");
+            insertTable("4", "Maarten");
 
             return null; // or an object for use outside the Steps
         };
@@ -98,7 +102,7 @@ public class IntegratieTest extends DBTestCase {
         PreparedStatement createPreparedStatement = null;
 
 //        String CreateQuery = "CREATE TABLE XZA_PERSOON(id number primary key, " +
-        String CreateQuery = "CREATE TABLE XZA_PERSOON(id int primary key, " +
+        String CreateQuery = "CREATE TABLE XZA_PERSOON(id number primary key, " +
                 "ACHTERNAAM varchar(255))";
 //                "BSNNUMMER varchar(255)," +
 //                "EMAIL varchar(255)," +
@@ -129,6 +133,76 @@ public class IntegratieTest extends DBTestCase {
         } finally {
             connection.close();
         }
+    }
+
+    public void selectTable() throws SQLException {
+        Connection connection = getDBConnection();
+        PreparedStatement createPreparedStatement = null;
+//        Persoon persoon = new Persoon();
+        String CreateQuery = "SELECT * FROM XZA_PERSOON";
+
+        try {
+            connection.setAutoCommit(false);
+//            ResultSet result = createPreparedStatement.executeQuery();
+
+            createPreparedStatement = connection.prepareStatement(CreateQuery);
+            ResultSet result = createPreparedStatement.executeQuery();
+            while (result.next()) {
+                Persoon persoon = new Persoon();
+                persoon.setId(result.getInt(1));
+                persoon.setAchterNaam(result.getString(2));
+                System.out.println("persoon is: " + persoon.toString());
+ //               mypersonen.add(persoon);
+            }
+            createPreparedStatement.close();
+
+        } catch (SQLException e) {
+            System.out.println("Exception Message " + e.getLocalizedMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            connection.close();
+        }
+    }
+
+
+    public void insertTable(String id, String achternaam) throws SQLException {
+//        String insert_sql = "INSERT INTO XZA_MYPERSOON (ID, ACHTERNAAM, BSNNUMMER, EMAIL, GESLACHT, TELEFOONNUMMER, TUSSENVOEGSELS, VOORLETTERS, HUISNUMMER, HUISNUMMERTOEVOEGING, LAND, POSTCODE, STRAAT, WOONPLAATS, GEBOORTEPLAATS, GEMEENTE) " +
+//                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        String insert_sql = "INSERT INTO XZA_PERSOON (ID, ACHTERNAAM) " +
+                "VALUES (?, ?)";
+
+//        Connection connection = null;
+        Connection connection = getDBConnection();
+        Persoon persoon = new Persoon();
+        try {
+            PreparedStatement statement = connection.prepareStatement(insert_sql);
+            if (statement != null) {
+//                persoon.setId(getMaxPersoonID(connection));
+//                statement.setInt(1, persoon.getId());
+                statement.setInt(1, 1);
+                statement.setString(2, "Simon");
+                try {
+                    int count = statement.executeUpdate();
+                    boolean ok = count == 1;
+                    //         if(!ok) id=0;
+                } catch (Exception statmentExcption) {
+                    statmentExcption.printStackTrace();
+                    statmentExcption.printStackTrace(); //return 0 ;
+                }
+                statement.close();
+            }
+        } catch (Exception generalException) {
+            generalException.printStackTrace();
+            generalException.printStackTrace(); //return 0;
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) { /* ignored */}
+            }
+        }
+ //       return persoon;
     }
 
 }
---
+++ src/test/java/nl/rabobank/personenbeheer/RestController.java
@@ -0,0 +1,7 @@
+package nl.rabobank.personenbeheer;
+
+/**
+ * Created by HoltropA on 12-7-2017.
+ */
+public @interface RestController {
+}
